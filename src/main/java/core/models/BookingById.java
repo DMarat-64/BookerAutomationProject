cc7e538f4665767e26c79a667e2ba59dede3a00d
Marat D <Marat@mail.ru>
2026-01-29 23:39:06 +0400

    added BookingById

---
+++ src/main/java/core/models/BookingById.java
@@ -0,0 +1,89 @@
+package core.models;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class BookingById {
+    public String firstname;
+    public String lastname;
+    public int totalprice;
+    public boolean depositpaid;
+    public Bookingdates bookingdates;
+    public String additionalneeds;
+
+    @JsonCreator
+    public BookingById(@JsonProperty("totalprice") int totalprice) {
+        this.totalprice = totalprice;
+    }
+
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getTotalprice() {
+        return totalprice;
+    }
+
+    public void setTotalprice(int totalprice) {
+        this.totalprice = totalprice;
+    }
+
+    public boolean isDepositpaid() {
+        return depositpaid;
+    }
+
+    public void setDepositpaid(boolean depositpaid) {
+        this.depositpaid = depositpaid;
+    }
+
+    public String getAdditionalneeds() {
+        return additionalneeds;
+    }
+
+    public void setAdditionalneeds(String additionalneeds) {
+        this.additionalneeds = additionalneeds;
+    }
+
+    public Bookingdates getBookingdates() {
+        return bookingdates;
+    }
+
+    public void setBookingdates(Bookingdates bookingdates) {
+        this.bookingdates = bookingdates;
+    }
+
+    public class Bookingdates{
+        public String checkin;
+        public String checkout;
+
+        public String getCheckin() {
+            return checkin;
+        }
+
+        public void setCheckin(String checkin) {
+            this.checkin = checkin;
+        }
+
+        public String getCheckout() {
+            return checkout;
+        }
+
+        public void setCheckout(String checkout) {
+            this.checkout = checkout;
+        }
+    }
+}
+

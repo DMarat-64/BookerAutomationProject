054380016ee22b6a8fc89083ddc9d489e467776d
Marat D <Marat@mail.ru>
2026-02-03 00:06:24 +0400

    add UpdateBookingTest

---
+++ src/test/java/tests/UpdateBookingTest.java
@@ -0,0 +1,98 @@
+package tests;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import core.clients.APIClient;
+import core.models.BookingDates;
+import core.models.CreateBooking;
+import core.models.NewCreatedBooking;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UpdateBookingTest {
+
+    private APIClient apiClient;
+    private ObjectMapper objectMapper;
+    private CreateBooking createdBooking;
+    private NewCreatedBooking newCreatedBooking;
+
+    @BeforeEach
+    public void setup() {
+        apiClient = new APIClient();
+        objectMapper = new ObjectMapper();
+
+        createdBooking = new CreateBooking();
+        createdBooking.setFirstname("Bob");
+        createdBooking.setLastname("Falker");
+        createdBooking.setTotalprice(135);
+        createdBooking.setDepositpaid(true);
+        createdBooking.setBookingdates(new BookingDates("2025-04-08", "2025-04-10"));
+        createdBooking.setAdditionalneeds("Breakfast");
+    }
+
+    @Test
+    @Feature("Booking")
+    @Severity(SeverityLevel.CRITICAL)
+    @Owner("marat")
+    public void testUpdateBooking() throws JsonProcessingException {
+
+        String requestBody = objectMapper.writeValueAsString(createdBooking);
+        Response response = apiClient.createBooking(requestBody);
+
+        assertThat(response.getStatusCode()).isEqualTo(200);
+
+        String responseBody = response.asString();
+        newCreatedBooking = objectMapper.readValue(responseBody, NewCreatedBooking.class);
+
+        assertThat(newCreatedBooking).isNotNull();
+        assertEquals(newCreatedBooking.getBooking().getFirstname(), createdBooking.getFirstname());
+        assertEquals(newCreatedBooking.getBooking().getLastname(), createdBooking.getLastname());
+        assertEquals(newCreatedBooking.getBooking().getTotalprice(), createdBooking.getTotalprice());
+        assertEquals(newCreatedBooking.getBooking().getBookingdates().getCheckin(), createdBooking.getBookingdates().getCheckin());
+        assertEquals(newCreatedBooking.getBooking().getAdditionalneeds(), createdBooking.getAdditionalneeds());
+
+        Integer bookingId = newCreatedBooking.getBookingid();
+        assertThat(bookingId).isNotNull().isGreaterThan(0);
+
+        apiClient.createToken("admin", "password123");
+
+        CreateBooking updateData = new CreateBooking();
+        updateData.setFirstname("Jonathan");
+        updateData.setLastname("Falker");
+        updateData.setTotalprice(500);
+        updateData.setDepositpaid(true);
+        updateData.setBookingdates(new BookingDates("2025-04-08", "2025-04-10"));
+        updateData.setAdditionalneeds("Breakfast");
+
+        String updateBody = objectMapper.writeValueAsString(updateData);
+        Response updateBookingId = apiClient.putBookingId(updateBody, bookingId);
+
+        String responseBodyFromPatch = updateBookingId.getBody().asString();
+        CreateBooking updateBooking = objectMapper.readValue(responseBodyFromPatch, CreateBooking.class);
+
+        assertThat(updateBookingId.getStatusCode()).isEqualTo(200);
+        assertThat(updateBooking).isNotNull();
+        assertEquals(newCreatedBooking.getBooking().getFirstname(), createdBooking.getFirstname());
+        assertEquals(newCreatedBooking.getBooking().getLastname(), createdBooking.getLastname());
+        assertEquals(newCreatedBooking.getBooking().getTotalprice(), createdBooking.getTotalprice());
+        assertEquals(newCreatedBooking.getBooking().getBookingdates().getCheckin(), createdBooking.getBookingdates().getCheckin());
+        assertEquals(newCreatedBooking.getBooking().getAdditionalneeds(), createdBooking.getAdditionalneeds());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        apiClient.createToken("admin", "password123");
+        apiClient.deleteBooking(newCreatedBooking.getBookingid());
+
+        assertThat(apiClient.getDeleteBookingId(newCreatedBooking.getBookingid()).getStatusCode()).isEqualTo(404);
+    }
+}
